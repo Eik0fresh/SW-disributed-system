8114eef3fabd24873dcd55fc2261d672aa97fb75
dongshew96 <dongshew96@mi.fu-berlin.de>
2022-06-01 10:36:01 +0200

    initial commit for patient-service

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# SW-disributed-system
-Repository for the software project „Distributed System“
+# Patient-Service
+Patient-service provides create, delete, update and query functions.
+## API
+Current requestbody and responsebody are only for presentation. A common response type might be needed later to communicate with client.
+### Create Patient
+| | Value |
+| ----------- | ----------- |
+| URL| http://localhost:8081/patient/create|
+| Method| POST |
+| Input | {"firstname": "Foo","surname": "Wang", "email": "wang@example.com"} |
+| Output| Message of success or fail|
+### Delete Patient
+| | Value |
+| ----------- | ----------- |
+| URL| http://localhost:8081/patient/delete/{id}|
+| Method| Get |
+| Input |  |
+| Output| Message of success or fail|
+### Query Patient'f Information
+| | Value |
+| ----------- | ----------- |
+| URL| http://localhost:8081/patient/query|
+| Method| POST |
+| Input | {"firstname": "Foo","surname": "Wang", "email": "wang@example.com"} |
+| Output| Patient information in JSON format {"p_id", "firstname","surname","email"}|
+### Update Patient's Email
+| | Value |
+| ----------- | ----------- |
+| URL| http://localhost:8081/patient/update|
+| Method| POST |
+| Input | {"firstname": "Foo","surname": "Wang", "email": "wang@example.com"} |
+| Output| Message of success or fail|
---
+++ patient-management/src/main/java/swp/charite/patientmanagement/service/PatientService.java
@@ -0,0 +1,57 @@
+package swp.charite.patientmanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import swp.charite.patientmanagement.dto.PatientDto;
+import swp.charite.patientmanagement.model.Patient;
+import swp.charite.patientmanagement.repository.PatientRepository;
+
+@Service
+public class PatientService {
+    
+    @Autowired
+    private PatientRepository patientRepository;
+    
+    public String addPatient(PatientDto patient) {
+        if (!patientRepository.existsByFirstnameAndSurname(patient.getFirstname(), patient.getSurname())) {
+            Patient newPatient = new Patient();
+            newPatient.setFirstname(patient.getFirstname());
+            newPatient.setSurname(patient.getSurname());
+            newPatient.setEmail(patient.getEmail());
+            patientRepository.save(newPatient);
+            return "Create patient successfully!";
+        } else {
+            return "Patient exists!";
+        }
+    }
+
+    public String updateEmail(PatientDto patient) {
+        if (!patientRepository.existsByFirstnameAndSurname(patient.getFirstname(), patient.getSurname())) {
+            return "No patient exists!";
+        } else {
+            Patient oldPatient = patientRepository.findByFirstnameAndSurname(patient.getFirstname(), patient.getSurname());
+            oldPatient.setEmail(patient.getEmail());
+            patientRepository.save(oldPatient);
+            return "Update email successfully!";
+        }
+    }
+
+    public Patient queryPatient(PatientDto patient) {
+        if (!patientRepository.existsByFirstnameAndSurname(patient.getFirstname(), patient.getSurname())) {
+            return null;
+        } else {
+            Patient oldPatient = patientRepository.findByFirstnameAndSurname(patient.getFirstname(), patient.getSurname());
+            return oldPatient;
+        }
+    }
+
+    public String deletePatient(long id) {
+        if (patientRepository.existsById(id)) {
+            patientRepository.deleteById(id);;
+            return "Delete patient successfully!";
+        } else {
+            return "Invalid patient ID.";
+        }
+    }
+}
