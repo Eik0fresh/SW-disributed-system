83e51d5ac9e436f1e2427673457d63da732cd629
Eike Taegener <e.taegener@t-online.de>
2022-06-09 19:59:42 +0200

    [inter-service-communication] Main achievement of this commit: If a patient is created, a Kafka message is created and consumed by the demo-service. The logic is not final. Can be changed, is only implemented to visualize the effect.
    Main implementation aspects:
    - Database of the patient modified (extended with an outbox table)
    - Debezium added (Database monitoring service, to detect changes within
      the outbox table)
    - Kafka-Broker added (Used to propagate the Kafka messages)
    - Demo-service extended with Kafka functionalities (Kafka config and
      Kafka consumer)
    - Using the Outbox Pattern and CDC (Change Data Capture)
    
    Important, necessary implementation:
    - The DebziumTransformer creates only messages, if an entry in the
      outbox table is created, If the entry is removed, the transformer does
      nothing.
    - The outbox table is used, so not every change within the patient
      database is propagated into the backend system. Otherwise for each
      database table a connector would be necessary.
    - Direct create and remove of the outbox entry is necessary, so that the
      outbox table does not consume storage space.
    - The logic of the Kafka consumer (demo-service) can be changed to do
      something useful
    - The DTO, which is delivered with the Kafka message, needs to be the
      same on both sides (sender and receiver).

--- patient-management/src/main/java/swp/charite/patientmanagement/service/PatientService.java
+++ patient-management/src/main/java/swp/charite/patientmanagement/service/PatientService.java
@@ -1,22 +1,44 @@
 package swp.charite.patientmanagement.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import swp.charite.patientmanagement.dto.PatientCreateEventDto;
 import swp.charite.patientmanagement.dto.PatientDto;
+import swp.charite.patientmanagement.model.OutboxEntity;
 import swp.charite.patientmanagement.model.Patient;
+import swp.charite.patientmanagement.repository.OutboxRepository;
 import swp.charite.patientmanagement.repository.PatientRepository;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 @Service
 public class PatientService {
-    
+
+    @Autowired
+    private ObjectMapper mapper;
+
     @Autowired
     private PatientRepository patientRepository;
-    
+
+    @Autowired
+    private OutboxRepository outboxRepository;
+
+    @Transactional
     public String addPatient(PatientDto patient) {
         if (!patientRepository.existsByFirstnameAndSurname(patient.getFirstname(), patient.getSurname())) {
             Patient newPatient = new Patient(null, patient.getFirstname(), patient.getSurname(), patient.getEmail());
             patientRepository.save(newPatient);
+
+            PatientCreateEventDto patientCreateEventDto = new PatientCreateEventDto(newPatient.getP_id(), newPatient.getFirstname(), newPatient.getSurname());
+            JsonNode jsonNode = mapper.convertValue(patientCreateEventDto, JsonNode.class);
+            OutboxEntity o = new OutboxEntity(UUID.randomUUID(), "patient", newPatient.getP_id().toString(), "patient_created", jsonNode.toString());
+            outboxRepository.save(o);
+            outboxRepository.delete(o);
+
             return "Create patient successfully!";
         } else {
             return "Patient exists!";
