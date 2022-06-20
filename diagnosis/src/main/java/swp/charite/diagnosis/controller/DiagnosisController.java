522e3b9d645e275b0351bbe5f2b5f64717e48d0d
Eike Taegener <eike.taegener@fu-berlin.de>
2022-06-20 13:54:10 +0200

    [Diagnosis] Modify RequestMapping of controller Set the RequestMapping to value="diagnosis" for all controllers. Gateway can redirect each HTTP-Request addressed to the diagnosis-service to it.
    Split the DiagnosisController into DiagnosisController and
    GuidanceController for better maintainability.

--- diagnosis/src/main/java/swp/charite/diagnosis/controller/DiagnosisController.java
+++ diagnosis/src/main/java/swp/charite/diagnosis/controller/DiagnosisController.java
@@ -3,11 +3,7 @@ package swp.charite.diagnosis.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import swp.charite.diagnosis.dto.GuidanceFromDoctor;
 import swp.charite.diagnosis.dto.GuidanceToPatient;
@@ -20,10 +16,8 @@ import swp.charite.diagnosis.service.GuidanceService;
 import swp.charite.diagnosis.service.PatientService;
 
 @RestController
+@RequestMapping(value = "diagnosis")
 public class DiagnosisController {
-
-    @Autowired
-    private GuidanceService guidanceService;
 
     @Autowired 
     private DiagnosisService diagnosisService;
@@ -33,29 +27,6 @@ public class DiagnosisController {
 
     @Autowired
     private DoctorService doctorService;
-
-    @PostMapping(value = "/guidance/create")
-    public ResponseEntity<String> createGuidance(@RequestBody GuidanceFromDoctor guidance) {
-        Boolean status = guidanceService.create(guidance);
-        if (status) {
-            return new ResponseEntity<String>("Create guidance successfully!", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<String>("Guidance exists!", HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping(value = "/guidance/query/{p_id}")
-    public ResponseEntity<GuidanceToPatient> queryGuidance(@PathVariable("p_id") Long p_id) {
-        Long dia_id = diagnosisService.findByPatient(p_id);
-        GuidanceToPatient guidance = guidanceService.query(dia_id);
-        return new ResponseEntity<GuidanceToPatient>(guidance, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "guidance/update/{g_id}")
-    public ResponseEntity<String> doneGuidance(@PathVariable("g_id") Long g_id) {
-        guidanceService.update(g_id);
-        return new ResponseEntity<String>("Guidance done!", HttpStatus.OK);
-    }
 
     @PostMapping(value = "/diagnosis/create")
     public ResponseEntity<String> createDiagnosis (@RequestBody Diagnosis diagnosis){
---
+++ diagnosis/src/main/java/swp/charite/diagnosis/controller/GuidanceController.java
@@ -0,0 +1,46 @@
+package swp.charite.diagnosis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import swp.charite.diagnosis.dto.GuidanceFromDoctor;
+import swp.charite.diagnosis.dto.GuidanceToPatient;
+import swp.charite.diagnosis.service.DiagnosisService;
+import swp.charite.diagnosis.service.GuidanceService;
+
+@Controller
+@RequestMapping(value = "diagnosis")
+public class GuidanceController {
+
+    @Autowired
+    private GuidanceService guidanceService;
+
+    @Autowired
+    private DiagnosisService diagnosisService;
+
+    @PostMapping(value = "/guidance/create")
+    public ResponseEntity<String> createGuidance(@RequestBody GuidanceFromDoctor guidance) {
+        Boolean status = guidanceService.create(guidance);
+        if (status) {
+            return new ResponseEntity<String>("Create guidance successfully!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("Guidance exists!", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/guidance/query/{p_id}")
+    public ResponseEntity<GuidanceToPatient> queryGuidance(@PathVariable("p_id") Long p_id) {
+        Long dia_id = diagnosisService.findByPatient(p_id);
+        GuidanceToPatient guidance = guidanceService.query(dia_id);
+        return new ResponseEntity<GuidanceToPatient>(guidance, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "guidance/update/{g_id}")
+    public ResponseEntity<String> doneGuidance(@PathVariable("g_id") Long g_id) {
+        guidanceService.update(g_id);
+        return new ResponseEntity<String>("Guidance done!", HttpStatus.OK);
+    }
+
+}
